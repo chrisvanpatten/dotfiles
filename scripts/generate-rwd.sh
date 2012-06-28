306e4396f24e1ca7be2437141dca543d27f9f100
Chris Van Patten <chris@vanpattenmedia.com>
2012-06-28 16:02:45 -0400

    Tweak to explicitly set browser width

--- scripts/generate-rwd.sh
+++ scripts/generate-rwd.sh
@@ -3,17 +3,17 @@
 # Assembled by Chris Van Patten, released into the wild 28 June 2012
 
 # Laptop: 1440x900
-webkit2png --clipped $1 --scale=1.0 --clipwidth=1440 --clipheight=900 --filename=laptop
+webkit2png --clipped $1 --scale=1.0 --width=1440 --height=900 --clipwidth=1440 --clipheight=900 --filename=laptop
 mv laptop-clipped.png laptop.png
 sips -z 169 268 laptop.png
 
 # Tablet: 1024x768
-webkit2png --clipped $1 --scale=1.0 --clipwidth=1024 --clipheight=768 --filename=tablet
+webkit2png --clipped $1 --scale=1.0 --width=1024 --height=768 --clipwidth=1024 --clipheight=768 --filename=tablet
 mv tablet-clipped.png tablet.png
 sips -z 125 165 tablet.png
 
 # Smartphone: 320x480
-webkit2png --clipped $1 --scale=1.0 --clipwidth=320 --clipheight=480 --filename=smartphone
+webkit2png --clipped $1 --scale=1.0 --width=320 --height=480 --clipwidth=320 --clipheight=480 --filename=smartphone
 mv smartphone-clipped.png smartphone.png
 sips -z 60 39 smartphone.png
 
