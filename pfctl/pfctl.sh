d0255a2ecdb1ee04d757033cc65a22cd827812e2
Chris Van Patten <chris@vanpattenmedia.com>
2015-04-21 19:37:45 +0200

    musicglove is now flintrehab

--- pfctl/pfctl.sh
+++ pfctl/pfctl.sh
@@ -75,7 +75,7 @@ elif [ $1 == 'stalkingthebogeyman' ]; then
 	sed 's/XXXX/15033/' com.vanpattenmedia.template > com.vanpattenmedia
 elif [ $1 == 'wiesenthaltheplay' ]; then
 	sed 's/XXXX/15034/' com.vanpattenmedia.template > com.vanpattenmedia
-elif [ $1 == 'musicglove' ]; then
+elif [ $1 == 'flintrehab' ]; then
 	sed 's/XXXX/15035/' com.vanpattenmedia.template > com.vanpattenmedia
 elif [ $1 == 'myblastoffapp' ]; then
 	sed 's/XXXX/15036/' com.vanpattenmedia.template > com.vanpattenmedia
